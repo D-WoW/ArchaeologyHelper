7210e6f73bef73b3de87d68a27d5b47c0f77d574
Janssen,Michael M.M <m.m.janssen@hotmail.com>
2020-08-25 16:19:25 +0200

    [INIT] Created base project
    Base setup includes:
    - pom file import of dependency json-simple
    - base classes without implementation
    - base resource json files without content

---
+++ src/main/java/dwow/archaeologyhelper/Faction.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dwow.archaeologyhelper;
+
+/**
+ *
+ * @author Michael
+ */
+public enum Faction {
+    AGNOSTIC("Agnostic"),
+    ZAROSIAN("Zarosian"),
+    ZAMORAKIAN("Zamorakian"),
+    ARMADYLEAN("Armadylean"),
+    BANDOSIAN("Bandosian");
+    
+    private final String name;
+    
+    private Faction(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
