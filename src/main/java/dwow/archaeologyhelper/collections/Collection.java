0bca64ebaab1d6b5390ca7b10ca1ffacc8e31939
Janssen,Michael M.M <m.m.janssen@hotmail.com>
2020-08-30 15:54:56 +0200

    [UPDATE] Updated the Collector and Collection class
    Implemented methods, added comments and implemented a bidirectional relationship between Collection and Collector where the Collector is the leader when it comes to adding collections to the list.
    
    Example:
    Collection class:
    void setCollector() is not publicly accessible, only package accessible, and it only sets the Collector for that collection.
    
    Collector class:
    public void addcollection() is publicly accessible and it adds a Collection to the list, while adding itself to that given Collection.

--- src/main/java/dwow/archaeologyhelper/collections/Collection.java
+++ src/main/java/dwow/archaeologyhelper/collections/Collection.java
@@ -5,10 +5,177 @@
  */
 package dwow.archaeologyhelper.collections;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  *
  * @author Michael
  */
 public class Collection {
     
+    /**
+     * Name of the Collection
+     */
+    private final String name;
+    
+    /**
+     * Minimum level required to start this Collection
+     */
+    private final int minlevel;
+    
+    /**
+     * Maximum level required to finish this Collection
+     */
+    private final int maxlevel;
+    
+    /**
+     * The Collector who you deliver this Collection to
+     */
+    private Collector collector;
+    
+    /**
+     * The Artefacts required to complete this Collection
+     */
+    private final List<Artefact> artefacts;
+
+    /**
+     * The one time reward for completing this Collection
+     */
+    private final String reward_once;
+    
+    /**
+     * The repeatable reward for completing this Collection: <String, Integer>
+     */
+    private final Map<String, Integer> reward_always;
+    
+    /**
+     * The constructor for initializing Collection objects
+     * @param name              The name of the Collection
+     * @param min               The minimum level required to start the Collection
+     * @param max               The maximum level required to finish the Collection
+     * @param artefacts         The Artefacts required to complete this Collection
+     * @param reward_once       The one time reward for completing this Collection
+     * @param reward_always     The repeatable reward for completing this collection
+     */
+    public Collection(
+            String name, 
+            int min, 
+            int max, 
+            List<Artefact> artefacts,
+            String reward_once,
+            Map<String, Integer> reward_always) {
+        this.name = name;
+        this.minlevel = min;
+        this.maxlevel = max;
+        this.artefacts = artefacts;
+        this.reward_once = reward_once;
+        this.reward_always = reward_always;
+    }
+    
+    /**
+     * Get the name of the Collection
+     * @return 
+     */
+    public String getName() {
+        return this.name;
+    }
+    
+    /**
+     * Get the minimum level required to start the Collection
+     * @return 
+     */
+    public int getMinimumLevel() {
+        return this.minlevel;
+    }
+    
+    /**
+     * Get the maximum level required to finish the Collection
+     * @return 
+     */
+    public int getMaximumLevel() {
+        return this.maxlevel;
+    }
+    
+    /**
+     * Get the Collector who you deliver this Collection to
+     * @return 
+     */
+    public Collector getCollector() {
+        return this.collector;
+    }
+    
+    /**
+     * Get a list of Artefacts required to finish this Collection
+     * @return 
+     */
+    public List<Artefact> getArtefacts() {
+        return this.artefacts;
+    }
+    
+    /**
+     * Get the one time reward of this Collection if there is one
+     * @return 
+     */
+    public String getOneTimeReward() {
+        return (this.reward_once == null 
+                ? "There is no one time reward" 
+                : this.reward_once);
+    }
+    
+    /**
+     * Get the repeatable reward of this Collection
+     * @return 
+     */
+    public Map<String, Integer> getRepeatableReward() {
+        return this.reward_always;
+    }
+    
+    /**
+     * Add a Collector to this Collection
+     * Do note that this is a bidirectional relationship
+     * The Collector class handles this so this method should
+     * not be used otherwise
+     * @param collector 
+     */
+    void setCollector(Collector collector) {
+        this.collector = collector;
+    }
+    
+    /**
+     * Verify if the given object is the same as the current object
+     * @param other
+     * @return 
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || !(other instanceof Collection)) return false;
+        
+        Collection collection = (Collection) other;
+        return (collection.name.equals(this.name) &&
+                collection.collector.equals(this.collector));
+    }
+
+    /**
+     * Get the hashCode for the Collection class
+     * @return 
+     */
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + Objects.hashCode(this.collector);
+        return hash;
+    }
+    
+    /**
+     * Get the visual String representation for the Collection class
+     * @return 
+     */
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
--- src/main/java/dwow/archaeologyhelper/collections/Collector.java
+++ src/main/java/dwow/archaeologyhelper/collections/Collector.java
@@ -5,10 +5,113 @@
  */
 package dwow.archaeologyhelper.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
  * @author Michael
  */
 public class Collector {
     
+    /**
+     * Name of the Collector
+     */
+    private final String name;
+    
+    /**
+     * Location of the Collector
+     */
+    private final String location;
+    
+    /**
+     * Collections the Collector collects
+     */
+    private final List<Collection> collections = new ArrayList();
+    
+    /**
+     * The constructor for initializing Collector objects
+     * @param name
+     * @param location
+     */
+    public Collector(
+            String name, 
+            String location) {
+        this.name = name;
+        this.location = location;
+    }
+    
+    /**
+     * Get the name of the Collector
+     * @return 
+     */
+    public String getName() {
+        return this.name;
+    }
+    
+    /**
+     * Get the location of the Collector
+     * @return 
+     */
+    public String getLocation() {
+        return this.location;
+    }
+    
+    /**
+     * Get the Collections the Collector collects
+     * @return 
+     */
+    public List<Collection> getCollections() {
+        return this.collections;
+    }
+    
+    /**
+     * Add a Collection to the list of the Collector
+     * Do note that this is a bidirectional relationship
+     * The Collector class does both the adding of a Collection
+     * and the adding of this Collector to the Collection object
+     * @param collection
+     */
+    public void addCollection(Collection collection) {
+        if (this.collections.contains(collection)) return;
+                
+        this.collections.add(collection);
+        collection.setCollector(this);
+    }
+    
+    /**
+     * Verify if the given object is the same as the current object
+     * @param other
+     * @return 
+     */
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || !(other instanceof Collector)) return false;
+        
+        Collector collector = (Collector) other;
+        return (collector.name.equals(this.name) &&
+                collector.location.equals(this.location));
+    }
+
+    /**
+     * Get the hashCode for the Collector class
+     * @return 
+     */
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + Objects.hashCode(this.location);
+        return hash;
+    }
+    
+    /**
+     * Get the visual String representation for the Collector class
+     * @return 
+     */
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
